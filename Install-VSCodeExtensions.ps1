2f8453846d610616358d48ea56766f1ddf6ac404
Christian Eiselt <dev@eiselt.ch>
2023-06-07 15:24:07 +0200

    update pipeline

--- Install-VSCodeExtensions.ps1
+++ Install-VSCodeExtensions.ps1
@@ -3,7 +3,7 @@ $AdditionalExtensions = (Get-Content ./package-lock.json | ConvertFrom-Json).ext
 if (!(Test-Path "$env:ProgramFiles\Microsoft VS Code\bin\code.cmd")) {
     Write-Host "`nDownloading latest Visual Studio Code (64-Bit)..." -ForegroundColor Yellow
     Remove-Item -Force "$env:TEMP\vscode-stable.exe" -ErrorAction SilentlyContinue
-    Invoke-WebRequest -Uri "https://vscode-update.azurewebsites.net/latest/win32-x64/stable" -OutFile "$env:TEMP\vscode-stable.exe"
+    Invoke-WebRequest -Uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-user" -OutFile "$env:TEMP\vscode-stable.exe"
 
     Write-Host "`nInstalling Visual Studio Code (64-Bit)..." -ForegroundColor Yellow
     Start-Process -Wait "$env:TEMP\vscode-stable.exe" -ArgumentList /silent, /mergetasks=!runcode
