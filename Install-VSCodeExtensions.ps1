84660d3afb7122cb03def4585882e13c3643dcff
christianeiselt <githubce@eiselt.ch>
2023-06-08 15:48:33 +0000

    update pipeline

--- Install-VSCodeExtensions.ps1
+++ Install-VSCodeExtensions.ps1
@@ -606,16 +606,7 @@ try {
         }
     }
 
-    $vscode_basename = (Get-Item "vscode\VSCode-win32-x64-*.zip").BaseName
-    $vscode_name = $($vscode_basename).Substring(0, $($vscode_basename).lastIndexOf('-'))
-    $vscode_version = "$($vscode_basename.Split('-')[-1])"
-    [System.Collections.ArrayList]$packages_list = @(
-        @{
-            "uid" = $vscode_name;
-            "version" = $vscode_version
-        }
-    )
-
+    [System.Collections.ArrayList]$extensions_list = @()
     # Archive each installed extension
     $extensions_installed = Get-ChildItem "C:\Users\runneradmin\.vscode\extensions"
     foreach ($ext_inst in $extensions_installed)
@@ -625,17 +616,23 @@ try {
             Write-Host $ext_inst.FullName
             $extension_name = $($ext_inst.Name).Substring(0, $($ext_inst.Name).lastIndexOf('-'))
             $extension_version = $ext_inst.Name.Split('-')[-1]
-            [System.Collections.HashTable]$extension_hashtable = @{
+            $extension_hashtable = @{
                 "uid" = $extension_name;
                 "version" = $extension_version };
-            [void]$packages_list.Add($extension_hashtable)
+            [void]$extensions_list.Add($extension_hashtable)
     
             Write-Host "Archiving version $extension_version of extension $extension_name ($($ext_inst.FullName))"
             Compress-Archive -Path $ext_inst.FullName -DestinationPath "./vscode/extensions/$($ext_inst.Name).zip"
         }
     }
 
-    $jsonRepresentation = $($packages_list | ConvertTo-Json)
+    $vscode_basename = (Get-Item "vscode\VSCode-win32-x64-*.zip").BaseName
+    $vscode_version = "$($vscode_basename.Split('-')[-1])"
+    [System.Collections.Hashtable]$packages = @{
+        "vscode_version" = "$vscode_version";
+        "extensions" = $extensions_list
+    }
+    $jsonRepresentation = $($packages.GetEnumerator() | Sort Name | ConvertTo-Json)
     Write-Host $jsonRepresentation
     $jsonRepresentation | Set-Content ".\packages.json"
 
